34e7de120b27f3f1f40ed2be1ed57cb2288d1500
ronnielopez <ronnielopez503@gmail.com>
2020-08-03 15:08:12 -0600

    docs agregado

---
+++ docs/precache-manifest.cb304a95a8e70ce27dcaaa3bf19bb924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17969cf51916d9acfbaf94b477f922c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1d9cc42e10faaa2d91d",
+    "url": "/static/css/main.2b8da10e.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1d9cc42e10faaa2d91d",
+    "url": "/static/js/main.3b2b6fee.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
